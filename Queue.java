bbe4cb28103c19b8fbe68cfc1e17fac8cbd08392
chitra623 <60198584+chitra623@users.noreply.github.com>
2022-05-09 13:10:09 -0400

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,6 +1,7 @@
 public class Queue extends SinglyLinkedList{
 
-	public Queue() {	
+	public Queue() {
+		
 	}
 	
 	public void enqueue(Currency object) {
@@ -13,40 +14,42 @@ public class Queue extends SinglyLinkedList{
 		setEnd(node1);
 		setCount(getCount() + 1);
 		
+		
 		//this.add(object);
     	}
 	
 	public Currency dequeue() {
 		if (isListEmpty()) {
-            		return null;
-        	}
+            return null;
+        }
 		Currency result = getStart().getData();
-        	setStart(getStart().getNext());
-        	setCount(getCount() - 1);
-        	if (isListEmpty()) {
-             	setEnd(null);
-        	}
-        	return result;
-    	}
+        setStart(getStart().getNext());
+        setCount(getCount() - 1);
+        if (isListEmpty()) {
+             setEnd(null);
+        }
+        return result;
+   	}
 	
 	public Currency peekFront() {
 		if (isListEmpty()) {
-            		return null;
-        	}
-        	return getStart().getData();
+            return null;
+        }
+        return getStart().getData();
 	}
 	
 	public Currency peekRear() {
 		if (isListEmpty()) {
-            		return null;
-        	}
-        	return getEnd().getData();
+            return null;
+        }
+        return getEnd().getData();
 	}
 	
 	public void printQueue()  {
 		LinkNode current = getStart();
 		while (current != null) {
-			System.out.println(current.getData() + " ");
+			current.getData().print();
+			System.out.print(current.getData().getNoteValue() + "." + current.getData().getCoinValue() + " ");
 			current = current.next;
 		}
     	}
