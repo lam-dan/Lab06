ad0b618e10e6a67b52508fff0c029304976e29e8
chitra623 <60198584+chitra623@users.noreply.github.com>
2022-05-09 09:10:51 -0400

    Update LinkNode.java

--- LinkNode.java
+++ LinkNode.java
@@ -9,32 +9,33 @@
 public class LinkNode {
 
 	private Currency data;
-	private LinkNode next;
+	public LinkNode next;
 	
 	// Default constructor
-	public LinkNode(Currency data, LinkNode next) {
-		this.setData(data);
-		this.setNext(next);
+	public LinkNode(Currency obj) {
+		this.setData(obj);
+		this.setNext(null);
 		
 	}
 	
-// Getters and Setters
-/**
- * The getters for the LinkNode class get & read the data of the node, and pointer to the next node, 
- * so that they can be manipulated in add/subtract/isEqual/isGreater methods, and elsewhere in the program.
- * 
- * Pre: None
- * Post: the data of the node 
- */
-
-/**
- * The setters for the LinkNode class update the data and position of the node and set them so that they can be manipulated 
- * in add/remove methods in the linked list, and elsewhere in the program.
- * 
- * Pre: none
- * Post: The data & position of the node in the linked list is updated/set.
- */
-
+	// Getters and Setters
+		/**
+		 * The getters for the LinkNode class get & read the data of the node, 
+		 * and pointer to the next node, so that they can be manipulated in add/subtract/isEqual/isGreater methods,
+		 * and elsewhere in the program.
+		 * 
+		 * Pre: None
+		 * Post: the data of the node 
+		 */
+		
+		/**
+		 * The setters for the LinkNode class update the data and position
+		 * of the node and set them so that they can be manipulated in add/remove methods in the linked list,
+		 * and elsewhere in the program.
+		 * 
+		 * Pre: none
+		 * Post: The data & position of the node in the linked list is updated/set.
+		 */
 	public Currency getData() {
 		return data;
 	}
@@ -46,7 +47,6 @@ public class LinkNode {
 	public LinkNode getNext() {
 		return next;
 	}
-	
 	public void setNext(LinkNode next) {
 		this.next = next;
 	}
