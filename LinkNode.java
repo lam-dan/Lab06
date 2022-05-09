86c2073f34950a58ed166cb7f72f7bf51bf7eff5
chitra623 <60198584+chitra623@users.noreply.github.com>
2022-05-08 23:15:59 -0400

    Update LinkNode.java

--- LinkNode.java
+++ LinkNode.java
@@ -18,22 +18,23 @@ public class LinkNode {
 		
 	}
 	
-	// Getters and Setters
-		/**
-		 * The getters for the LinkNode class get & read the data of the node, and pointer to the next node, 
-     * so that they can be manipulated in add/subtract/isEqual/isGreater methods, and elsewhere in the program.
-		 * 
-		 * Pre: None
-		 * Post: the data of the node 
-		 */
-		
-		/**
-		 * The setters for the LinkNode class update the data and position of the node and set them so that they can be manipulated 
-     * in add/remove methods in the linked list, and elsewhere in the program.
-		 * 
-		 * Pre: none
-		 * Post: The data & position of the node in the linked list is updated/set.
-		 */
+// Getters and Setters
+/**
+ * The getters for the LinkNode class get & read the data of the node, and pointer to the next node, 
+ * so that they can be manipulated in add/subtract/isEqual/isGreater methods, and elsewhere in the program.
+ * 
+ * Pre: None
+ * Post: the data of the node 
+ */
+
+/**
+ * The setters for the LinkNode class update the data and position of the node and set them so that they can be manipulated 
+ * in add/remove methods in the linked list, and elsewhere in the program.
+ * 
+ * Pre: none
+ * Post: The data & position of the node in the linked list is updated/set.
+ */
+
 	public Currency getData() {
 		return data;
 	}
@@ -45,6 +46,7 @@ public class LinkNode {
 	public LinkNode getNext() {
 		return next;
 	}
+	
 	public void setNext(LinkNode next) {
 		this.next = next;
 	}
